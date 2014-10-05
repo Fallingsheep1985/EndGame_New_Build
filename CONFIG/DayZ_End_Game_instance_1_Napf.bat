1059e4deab271aac403958d3a2af61404f9cafa6
Fallingsheep1985 <lee@integr8it.com.au>
2014-10-06 08:37:03 +1100

    Config files

---
+++ CONFIG/DayZ_End_Game_instance_1_Napf.bat
@@ -0,0 +1,2 @@
+@echo off
+start "arma2" /min "Expansion\beta\arma2oaserver.exe" -port=2302 "-config=instance_1_Napf\config.cfg" "-cfg=instance_1_Napf\basic.cfg" "-profiles=instance_1_Napf" -name=instance_1_Napf "-mod=@Custommod1;@Custommod2;"
